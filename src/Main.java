ba32c63098c8c0b15b77a376e92146d28fcdd3a8
behniyaesm <behniyaesm@gmail.com>
2024-09-23 22:11:28 -0400

    Completed task 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+    int num1 = 4;
+    int num2 = 41;
+
+    if (num1 % 2 == 0 ) {
+        System.out.println(num1 + " is even");
+    }
+    else { System.out.println(num2 + " is odd");
+    }
+    if (num2 % 2 == 0 ) {
+        System.out.println(num1 + " is even");
+    }
+    else { System.out.println(num2 + " is odd");
+    }
+
     }
 }
